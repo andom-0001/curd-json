9cef1ff785475d67e05db593d90c77da79299794
andom-0001 <anandkumar241103@gmail.com>
2026-04-08 08:20:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+
+const file = "data.json";
+
+const readData = () => {
+    return JSON.parse(fs.readFileSync(file));
+}
+
+const writeData = (data) => {
+    fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+app.post('/users', (req, res) => {
+    const users = readData();
+    const { name, email, age, city } = req.body;
+    if (!name || !email){
+        return res.status(400).send({ error: "Name and Email are required" });
+    }
+    const dataexist = users.find(key => key.email === email);
+    if (dataexist) {
+        return res.status(400).send({ error: "User with this email already exists" });
+    }
+    const newUser = {
+        id: users.length ? users[users.length].id + 1 : 1,
+        name,
+        email,
+        age,
+        city
+    };
+    users.push(newUser);
+    writeData(users);
+    res.status(201).json(newUser);
+})
+
+app.get('/users', (req, res) => {
+    const users = readData();
+    let result = users;
+    res.json(result);
+});
+
+app.get('/users/:id', (req, res) => {
+    const users = readData();
+    const ids= users.find(key => key.id == req.params.id);
+    if (!ids) {
+        return res.status(404).send({ error: "User not found" });
+    }
+    res.json(ids);
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port  http://localhost:${PORT}`);
+});
